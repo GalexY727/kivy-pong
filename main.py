8c2c455c2f75e6776f1b1de8ed94ea0919e8dd1d
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-06-28 13:45:03 -0700

    it moves!

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
 
 class PongBall(Widget):
 
     # velocity of the ball on x and y axis
-    velocity_x = NumericProperty(0)
+    velocity_x = NumericProperty(1)
     velocity_y = NumericProperty(0)
 
     # referencelist property so we can use ball.velocity as
@@ -19,12 +20,25 @@ class PongBall(Widget):
         self.pos = Vector(*self.velocity) + self.pos
 
 class PongGame(Widget):
-    pass
+    ball = ObjectProperty(None)
+
+    def update(self, dt):
+        self.ball.move()
+
+        # bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        # bounce off left and right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
 
 class PongApp(App):
-    def build(self):
-        return PongGame()
 
+    def build(self):
+        game = PongGame()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
 
 if __name__ == '__main__':
     PongApp().run()
